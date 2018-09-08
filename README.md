5745bb57cf99afa89d03712a9cbd0c6719e2878d
Ertan Hasani <ertanhasani96@gmail.com>
2018-09-09 00:13:45 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# jsSlider
+jsSlider is a jQuery Slider Plugin that works with Bootstrap (v.3).
+
+Give it a height in pixels (number) and it will center all images either if they are smaller or not.
+
+# How to use it
+> In order to use it, you should already have imported `Bootstrap v3 CSS` and `jQuery` in your project.
+
+> CURRENTLY SUPPORTED ONLY FOR GOOGLE CHROME!
+
+Add a `div` inside your with an `id` or a `particular class`. Ex. 
+```
+<div id='mySlider'></div>
+```
+
+Then in you javascript add an create an array that will contain objects with `src` and `alt` properties. Ex.
+```
+var images = [
+    {src: 'images/image1.jpg', alt: 'image1'},
+    {src: 'images/image2.jpg', alt: 'image2'},
+    {src: 'images/image3.jpg', alt: 'image3'},
+    {src: 'images/image4.jpg', alt: 'image4'},  
+    {src: 'images/image5.jpg', alt: 'image5'}
+]
+```
+
+Then just call `jsSlider` function to that `div`. `jsSlider` function takes two parametres: first one takes the object with the images, and the second one takes height (in pixels) of slider (like how many pixels do you want its height).
+```
+$('#mySlider').jsSlider(images, 500);
+```
+
+And then it will look like this:
+![Home Page](https://image.ibb.co/fv45up/image1.jpg)
+
+
+Here's what it looks like on a real project
+![Home Page](https://image.ibb.co/mnXm0U/image2.jpg)
+![Home Page](https://image.ibb.co/epKAS9/image3.jpg)
---
+++ jsSlider.js
@@ -0,0 +1,193 @@
+jQuery.fn.extend({
+    jsSlider: function(images, height) {
+
+        var elm = $(this);
+
+        addCss();
+        $.when(addElement()).then(function(){
+            addMargin();
+        });
+        
+
+        function addElement()
+        {
+            //add images to element
+            var html = '';
+
+            html += '<div class="col-md-12 imagesWrapper text-center">';
+                html += '<a target="_blank" href="' + images[0].src + '" class="viewFull">VIEW</a>';
+
+                html += '<div class="goToPreviousImage">';
+                    html += '<label class="previous inactive">❮</label>';
+                html += '</div>';
+
+                $.each(images, function(i ,item){
+                    html += '<div class="imageWrapper ' + (i == 0 ? "active" : "nextImage") + '" data-id="' + (i + 1) + '">';
+                        html += '<img class="img-responsive" src="' + item.src + '" alt="' + item.alt + '" />';
+                    html += '</div>';
+                });
+
+                html += '<div class="goToNextImage">';
+                    html += '<label class="next ' + (images.length > 1 ? "" : "inactive") + '">❯</label>';
+                html += '</div>';
+
+            html += '</div>';
+
+            $(elm).html(html);
+            //end add images to element
+        }
+
+        function addMargin(){
+             //adding margin to position in center
+            var allImages = $(elm).find('.imagesWrapper img');
+
+            allImages.each(function (i, item) {
+                var imageHeight = item.height;
+                if (imageHeight < height) {
+                    $(item).css('margin-top', parseFloat((height - imageHeight) / 2));
+                }
+            });
+            //end adding margin to position in center
+        }
+
+        function addCss()
+        {
+            //add CSS
+            var css = '<style> \n' + 
+            '.imagesWrapper .imageWrapper img { \n\n' +
+                'max-height: \n' + height + 'px;\n\n' +
+                'display: inline;\n\n' +
+            '}\n' +
+        
+            '.imagesWrapper {\n' +
+                'position: relative;\n' +
+                'overflow: hidden;\n' +
+                'height: \n' + height + 'px;\n' +
+            '}\n' +
+        
+            '.imagesWrapper .imageWrapper {\n' +
+                'position: absolute;\n' +
+                'transition: 1.5s all ease-in-out;\n' +
+                'width: -webkit-fill-available;\n' +
+            '}\n' +
+
+            '.imageWrapper.previousImage {\n' +
+                'transform: translateX(-100%);\n' +
+                'left: 0;\n' +
+            '}\n' +
+        
+            '.imageWrapper.nextImage {\n' +
+                'transform: translateX(100%);\n' +
+                'left: 0;\n' +
+            '}\n' +
+        
+            '.imageWrapper.active {\n' +
+                'transform: translateX(0);\n' +
+                'left: 0;\n' +
+            '}\n' +
+        
+            '.goToNextImage {\n' +
+            'position: absolute;\n' +
+                'height: \n' + height + 'px;\n' +
+                'transform: translateY(42%);\n' +
+                'right: 0;\n' +
+                'z-index: 1;\n' +
+            '}\n' +
+        
+            '.goToPreviousImage {\n' +
+                'position: absolute;\n' +
+                'height: \n' + height + 'px;\n' +
+                'transform: translateY(42%);\n' +
+                'left: 0;\n' +
+                'z-index: 1;\n' +
+            '}\n' +
+        
+            '.previous {\n' +
+                'cursor: pointer;\n' +
+                'padding: 5px;\n' +
+                'font-size: 45px;\n' +
+                'background: #ffffff;\n' +
+            '}\n' +
+        
+            '.next {\n' +
+                'cursor: pointer;\n' +
+                'padding: 5px;\n' +
+                'font-size: 45px;\n' +
+                'background: #ffffff;\n' +
+            '}\n' +
+        
+            '.next.inactive, .previous.inactive {\n' +
+                'visibility: hidden;\n' +
+                'opacity: 0;\n' +
+            '}\n' +
+            '.viewFull {\n' +
+                'position: absolute;\n' +
+                'right: 0;\n' +
+               'cursor: pointer;\n' +
+                'z-index: 1;\n' +
+            '}\n' +
+        
+            '.viewFull:hover {\n' +
+                'text-decoration: none;\n' +
+            '}\n' +
+            '</style>';
+
+            $('html head').append(css);
+            //end adding CSS
+        }
+
+        $(function(){
+            var current = 1;
+
+            $(elm).on('click', '.previous', function () {
+                if (current == 1) {
+                    return false;
+                }
+
+                var active = $(elm).find('.imagesWrapper .imageWrapper.active');
+                
+                if (current == 2) {
+                    $(elm).find('.previous').addClass('inactive');
+                }
+
+                if (current <= images.length) {
+                    $(elm).find('.next').removeClass('inactive');
+                }
+
+                var previous = $(elm).find('.imagesWrapper .imageWrapper[data-id=' + (current - 1) + ']');
+
+                active.addClass('nextImage');
+                active.removeClass('active');
+                $(previous).addClass('active');
+                $(previous).removeClass('previousImage');
+
+                $(elm).find('.viewFull').attr('href', previous.children('img').attr('src'));
+                current--;
+            });
+
+            $(elm).on('click', '.goToNextImage', function () {
+                if (current == images.length) {
+                    return false;
+                }
+                
+                var active = $(elm).find('.imagesWrapper .imageWrapper.active');
+
+                if (current == images.length - 1) {
+                    $(elm).find('.next').addClass('inactive');
+                }
+
+                $(elm).find('.previous').removeClass('inactive')
+                var next = $(elm).find('.imagesWrapper .imageWrapper[data-id=' + (current + 1) + ']');
+                
+                active.addClass('previousImage');
+                active.removeClass('active');
+                $(next).addClass('active');
+                $(next).removeClass('nextImage');
+
+                $(elm).find('.viewFull').attr('href', next.children('img').attr('src'));
+                current++;
+            });
+        });
+    }
+  });
+   
